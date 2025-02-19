9c2232192dadd385268a0588740d571058485c4b
Kim Escabarte <kimescabarte@gmail.com>
2025-02-19 08:32:59 +0800

    Update Empty State

--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -12,6 +12,7 @@ import { MatPaginator, PageEvent } from '@angular/material/paginator'
 import { ShopParams } from '../../shared/models/shopParams';
 import { Pagination } from '../../shared/models/pagination';
 import { FormsModule } from '@angular/forms';
+import { EmptyStateComponent } from "../../shared/components/empty-state/empty-state.component";
 
 @Component({
   selector: 'app-shop',
@@ -24,8 +25,9 @@ import { FormsModule } from '@angular/forms';
     MatListOption,
     MatMenuTrigger,
     MatPaginator,
-    FormsModule
-  ],
+    FormsModule,
+    EmptyStateComponent
+],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
 })
@@ -100,5 +102,10 @@ export class ShopComponent implements OnInit {
         }
       }
     })
+  }
+
+  resetFilters() {
+    this.shopParams = new ShopParams();
+    this.getProducts();
   }
 }
