67c59eaf1086696a61cd386d49447230a007153f
Kim Escabarte <kimescabarte@gmail.com>
2025-04-28 15:55:08 +0800

    Frontend updates

---
+++ API/wwwroot/chunk-ZR73NPIL.js
@@ -0,0 +1,1 @@
+import{b as u}from"./chunk-QF4QJE7T.js";import{j as o}from"./chunk-JCB2LSZI.js";import{ia as t,q as e,w as n}from"./chunk-553H3WQF.js";var A=(m,a)=>{let r=t(u),c=t(o);return r.currentUser()?e(!0):r.getAuthState().pipe(n(i=>i.isAuthenticated?!0:(c.navigate(["account/login"],{queryParams:{returnUrl:a.url}}),!1)))};export{A as a};
